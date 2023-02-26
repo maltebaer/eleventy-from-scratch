c4f701a6ac01e95200922f8734afb25f33e37221
Malte Bär <maltebaer@posteo.de>
2023-02-26 20:56:54 +0100

    finish: lesson 17 (html done)

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const rssPlugin = require('@11ty/eleventy-plugin-rss');
+
 // Filters
 const dateFilter = require('./src/filters/date-filter.js');
 const w3DateFilter = require('./src/filters/w3-date-filter.js');
@@ -5,6 +7,9 @@ const w3DateFilter = require('./src/filters/w3-date-filter.js');
 require('./src/utils/sort-by-display-order.js');
 
 module.exports = config => {
+    // Plugins
+    config.addPlugin(rssPlugin);
+
     // Add filters
     config.addFilter('dateFilter', dateFilter);
     config.addFilter('w3DateFilter', w3DateFilter);
@@ -27,6 +32,13 @@ module.exports = config => {
     // Returns a collection of blog posts in reverse date order
     config.addCollection('blog', collection => {
         return [...collection.getFilteredByGlob('./src/posts/*.md')].reverse();
+    });
+
+    // Returns a list of people ordered by filename
+    config.addCollection('people', collection => {
+        return collection.getFilteredByGlob('./src/people/*.md').sort((a, b) => {
+            return Number(a.fileSlug) > Number(b.fileSlug) ? 1 : -1;
+        });
     });
 
     // Set directories to pass through to the dist folder
